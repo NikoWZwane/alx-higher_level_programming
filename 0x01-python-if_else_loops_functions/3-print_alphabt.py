d2b2fc101293bb33602297c63b00fcce6c262580
NikoWZwane <williezn92@gmail.com>
2024-06-02 08:38:14 -0700

    prints all possible different combinations of two digits.

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,6 +3,4 @@ output = ''
 for char in range(ord('a'), ord('z') + 1):
     if chr(char) not in ['q', 'e']:
         output += chr(char)
-
-print(f"{output}", end='')
-
+print("{}".format(output), end='')
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for two_digits in range(0, 10):
     for combi in range(two_digits + 1, 10):
         if two_digits == 8 and combi == 9:
-            print ('89')
+            print('89')
         else:
-            print ('{}{}, '.format(two_digits, combi), end='')
+            print('{}{}, '.format(two_digits, combi), end='')
