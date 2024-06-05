4bd48eae23796895d82f261159d987739f5e1179
NikoWZwane <williezn92@gmail.com>
2024-06-05 09:17:04 -0700

    element from a list like in C

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    # Check if idx is within the valid range
     if 0 <= idx < len(my_list):
         return my_list[idx]
     else:
-        # Return None or raise an exception if idx is out of bounds
         return None
