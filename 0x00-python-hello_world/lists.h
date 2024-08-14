55c3020cac1de197b9713bf06d549d3abf315ea7
NikoWZwane <williezn92@gmail.com>
2024-08-14 08:04:21 -0700

    Compile

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,7 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio>
+#include <stdlib.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 
 typedef struct listint_s
 {
