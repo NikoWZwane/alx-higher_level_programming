7b45a0691aa0aa5b7f1ac98ac6a57a6512bf3fdb
NikoWZwane <williezn92@gmail.com>
2024-08-14 05:57:28 -0700

    that removes all characters c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    return my_string.translate({ord("c"): None})
+    if my_string[:]:
+        remove_string1 = my_string.translate({ord("c"): None})
+        remove_string2 = remove_string1.translate({ord("C"): None})
+        return remove_string2
+    return my_string
