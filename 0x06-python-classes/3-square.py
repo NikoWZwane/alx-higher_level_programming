fc30d421fc8c0b0069d7cd0027e2c264a0f082e0
NikoWZwane <williezn92@gmail.com>
2024-09-17 04:59:21 -0700

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -20,7 +20,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
-        """area of square 
+        """area of square
         Returns:
             the size of square
         """
