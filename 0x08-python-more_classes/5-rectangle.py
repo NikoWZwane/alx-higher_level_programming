67cc2d030f1a59a2f016dfe0a0736c4f01226bfe
NikoWZwane <williezn92@gmail.com>
2024-09-25 11:59:45 -0700

    Eval is magic

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,14 +1,21 @@
+#!/usr/bin/python3
+"""class Rectangle"""
+
+
 class Rectangle:
+"""A class to represent a rectangle."""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Gets the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets the width of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -17,10 +24,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Gets the height of the rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Sets the height of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -28,14 +37,17 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """instance method that returns the rectangle area"""
         return self.width * self.height
 
     def perimeter(self):
+        """Public instance method that returns the rectangle perimeter"""
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
 
     def __str__(self):
+        """String representation"""
         if self.width == 0 or self.height == 0:
             return ""
         return "\n".join(["#" * self.width for _ in range(self.height)])
